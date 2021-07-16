09476c5ccd456ba824c9d23ee6e226ce1b609cf1
Mrl260 <72027522+Mrl260@users.noreply.github.com>
2021-07-15 21:11:25 -0300

    Add files via upload

---
+++ Arvores/abb_privado.c
@@ -0,0 +1,13 @@
+#include "abb_privado.h"
+
+/* Coloque abaixo as suas implementações */
+
+
+
+void abb_destroi_rec (no_t *n)
+{
+	if (n==NULL) return;
+	abb_destroi_rec (n->dir);
+	abb_destroi_rec (n->esq);
+	free(n);
+}
